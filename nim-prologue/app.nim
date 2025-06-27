9e3f469fe46e852c790e2b29d856e7681041767c
bearpipy <103978131+bearpipy@users.noreply.github.com>
2025-06-27 13:42:34 +0800

    change litestar project

--- nim-prologue/app.nim
+++ nim-prologue/app.nim
@@ -1,8 +1,12 @@
 import prologue
-import prologue/middlewares
+import std/strformat
 
 proc hello*(ctx: Context) {.async.} =
   resp "<h1>Hello, Prologue!</h1>"
+
+proc try_str_format(alice: string) {.async.} =
+  let name: string = fmt"My name is {alice}"
+  echo name
 
 let app = newApp()
 app.addRoute("/", hello)
